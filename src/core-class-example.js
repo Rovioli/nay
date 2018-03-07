9e54268f0c980cdf4e507a18d1cdfe85bdc75859
Remi <riseremi@icloud.com>
2018-03-07 18:45:43 +0300

    Add module class example

---
+++ src/core-class-example.js
@@ -0,0 +1,27 @@
+class Module {
+  constructor(command) {
+    if (!command || typeof command !== 'string') {
+      throw Error('Command must be a string.');
+    }
+    this.command = command;
+    this.enabled = true;
+  }
+
+  onReceive() {
+    console.log('onReceive');
+  }
+
+  onEnable() {
+    console.log('onEnable');
+  }
+
+  onDisable() {
+    console.log('onDisable');
+  }
+}
+
+class SupaModule extends Module {
+  constructor(command) {
+    super(command);
+  }
+}
