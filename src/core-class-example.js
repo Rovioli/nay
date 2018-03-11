fbf8e2e60ce72973c75d6d90fb542bb46db5721e
Vitalii Dmitriev <dmit.vitalii@gmail.com>
2018-03-11 20:30:11 +0300

    Add a proposal for OOP-styled API.

--- src/core-class-example.js
+++ src/core-class-example.js
@@ -1,15 +1,29 @@
 class Module {
-  onReceive(sender, args = []) {
+
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  onReceive(args = []) {
     console.log('onReceive');
   }
 
-  onEnable(sender) {
+  onEnable() {
     console.log('onEnable');
   }
 
-  onDisable(sender) {
+  onDisable() {
     console.log('onDisable');
   }
 }
 
-class SupaModule extends Module {}
+class SupaModule extends Module {
+  onReceive(args = []) {
+    console.log('onReceive: with args ' + args);
+    this.sender.send('I never asked for this.');
+  }
+}
+
+// core.js
+const modules = new Map();
+map.set('/superstart', new SupaModule({send:args => console.log(args);}));
